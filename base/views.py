f9b9b9789e5aa4a6f8985d0945637248040b66a6
JesseAger <agerjesse6@gmail.com>
2024-04-22 15:46:22 +0300

    Introduction to django

---
+++ base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
+def home(request):
+    return render(request, 'home.html', {'rooms':rooms} )
+
+def room(request):
+    return render(request, 'room.html')
+
+
