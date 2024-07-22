2da6f10ac33a88e4dc250d96090cc8b8c02bbfe4
JesseAger <agerjesse6@gmail.com>
2024-07-23 01:14:32 +0300

    user model

--- src/base/models.py
+++ src/base/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
 class Room(models.Model):
     # host =
-    # topic =
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL)
     name = models.CharField(max_length=120)
     description = models.TextField(null=True, blank= True)
     # participants =
@@ -15,7 +24,7 @@ class Room(models.Model):
         return self.name
     
 class Message(models.Model):
-    # user =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     #Setting the foreign key: messages being child to Room model; on_delete=models.CASCADE means if the parent model is deleted, all the Messages in that ROoom gets deleted too
     body= models.TextField()
