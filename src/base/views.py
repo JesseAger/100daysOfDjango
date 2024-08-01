ec65a88afe28804efc781be8424ccc4c6a5e21b9
JesseAger <agerjesse6@gmail.com>
2024-08-01 14:06:58 +0300

    views

--- src/base/views.py
+++ src/base/views.py
@@ -17,26 +17,22 @@ from django.contrib.auth.forms import UserCreationForm
 
 def loginPage(request):
     page = 'login'
-
-    if request.user.is_authenticated:
-        return redirect('home')
-
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'User does not exist')
-
         user = authenticate(request, username=username, password=password)
+        user is not None
 
-        if user is not None:
-            login(request, user)
-            return redirect ('home')
-        else:
-            messages.error(request, "User does not exist")
+    if request.user.is_authenticated:
+        return redirect('home')
+
+
+    #     if user is not None:
+    #         login(request, user)
+    #         return redirect ('home')
+    #     else:
+    #         messages.error(request, "User does not exist")
 
     context = {'page': page}
     return render(request, 'base/login_register.html', context)
