7f74fde744d950882fc5d02b2340ae73ddb23ef3
JesseAger <agerjesse6@gmail.com>
2024-08-02 13:02:24 +0300

    templates1

--- src/base/views.py
+++ src/base/views.py
@@ -25,12 +25,13 @@ def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+
         try:
             user = User.objects.get(username= username)
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
