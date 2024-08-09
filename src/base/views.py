9461c7595b50c1595412ff9630efdf8dc0c6a20d
JesseAger <agerjesse6@gmail.com>
2024-08-09 12:45:45 +0300

    create-room

--- src/base/views.py
+++ src/base/views.py
@@ -101,22 +101,37 @@ def userProfile(request, pk):
     context = {'user':user, 'rooms':rooms, 'room_message': room_message, 'topics':topics}
     return render(request, 'base/profile.html', context)
 
+# @login_required(login_url='login')
+# def createRoom(request):
+#     form = RoomForm()
+
+#     if request.method == 'POST':
+#         form = RoomForm(request.POST)
+#         for room in Room.objects.all():
+
+#             if request.user != room.host:
+#                 return HttpResponse("You don't have permissions to complete your action")
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect('home')
+
+#     context = {'form': form}
+#     return render(request, 'base/room_form.html', context)
 @login_required(login_url='login')
 def createRoom(request):
-    form = RoomForm()
-
     if request.method == 'POST':
         form = RoomForm(request.POST)
-        for room in Room.objects.all():
-
-            if request.user != room.host:
-                return HttpResponse("You don't have permissions to complete your action")
-            if form.is_valid():
-                form.save()
-                return redirect('home')
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user  # Set the current user as the host
+            room.save()
+            return redirect('home')
+    else:
+        form = RoomForm()
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
