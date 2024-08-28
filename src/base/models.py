0aa71ae072840b5fa025f6bd03e3f1ca14e37498
JesseAger <agerjesse6@gmail.com>
2024-08-28 12:02:51 +0300

    add resources

--- src/base/models.py
+++ src/base/models.py
@@ -40,4 +40,12 @@ class Message(models.Model):
         
     def __str__(self):
         return self.body[0:50]
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default= 'default.jpg', upload_to='profile-pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
 
--- src/base/views.py
+++ src/base/views.py
@@ -101,22 +101,9 @@ def userProfile(request, pk):
     context = {'user':user, 'rooms':rooms, 'room_messages': room_messages, 'topics':topics}
     return render(request, 'base/profile.html', context)
 
-# @login_required(login_url='login')
-# def createRoom(request):
-#     form = RoomForm()
-
-#     if request.method == 'POST':
-#         form = RoomForm(request.POST)
-#         for room in Room.objects.all():
-
-#             if request.user != room.host:
-#                 return HttpResponse("You don't have permissions to complete your action")
-#             if form.is_valid():
-#                 form.save()
-#                 return redirect('home')
-
-#     context = {'form': form}
-#     return render(request, 'base/room_form.html', context)
+def Resources(request, pk):
+    context = {}
+    return(request, 'base/resource.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
